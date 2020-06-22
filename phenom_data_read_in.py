8791e556e8d7e173bbbff17629275bcdd248765c
michaelmelquiades <54324820+michaelmelquiades@users.noreply.github.com>
2020-06-22 18:22:14 +0100

    Added numpy function
    There is now an option to use one of the function to simply read in the data and output the data and column names. One must do something like:
    data, col_names = csv_to_np(filepath)

--- phenom_data_read_in.py
+++ phenom_data_read_in.py
@@ -36,3 +36,38 @@ def csv_to_df(directory):
     df.to_pickle(p_path)
     
     return df
+##############################################################################################
+
+def csv_to_np(directory):
+    data = []    
+    for fname in os.scandir(directory):
+        with open(fname, 'r') as file:
+            for line in file.readlines():
+                line = line.replace(';', ',')
+                line = line.rstrip(',\n')
+                line = line.split(',')
+                data.append(line)
+    
+    #Find the longest line in the data 
+    longest_line = max(data, key = len)
+        
+    #Set the maximum number of columns
+    max_col_num = len(longest_line)
+
+    #Set the columns names
+    col_names = ['event_ID', 'process_ID', 'event_weight', 'MET', 'MET_Phi']
+
+    for i in range(1, (int((max_col_num-5)/5))+1):
+        col_names.append('obj'+str(i))
+        col_names.append('E'+str(i))
+        col_names.append('pt'+str(i))
+        col_names.append('eta'+str(i))
+        col_names.append('phi'+str(i))
+
+    #Convert everything into numpy arrays
+    data = np.array(data)
+    col_names = np.array(col_names)
+    
+    #Here's where my h5 code would go IF I HAD ANY
+    
+    return data, col_names
